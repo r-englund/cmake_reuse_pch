0ceaa530c14983a828009b5a00a7e2f8af60e65a
Rickard Englund <rickard@r-englund.com>
2019-09-10 17:40:16 +0200

    Test building on unix with  both ninja and make

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 
-mkdir -p build-unix
-cd build-unix
+mkdir -p build-make
+mkdir -p build-ninja
+
+cd build-make
 /mnt/e/github-master-repo/ext/cmake/install-pch-unix/bin/cmake -G "Unix Makefiles" ..
 make 
+
+
+cd ../build-ninja
+/mnt/e/github-master-repo/ext/cmake/install-pch-unix/bin/cmake -G "Ninja" ..
+ninja 
